28ea4d6f4b42a100bcd286d3d78364d8acac9ea7
AldaPro777 <1aaldahirmolinamedina@gmail.com>
2024-10-10 18:35:43 -0600

    waddwawda

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
-    port: process.env.PORT || 7777,
+    port: process.env.PORT || 7777, // Usa la variable de entorno PORT en Render
   },
+  build: {
+    outDir: "dist", // Asegúrate de que Render publique desde el directorio dist
+  },
+  base: "/", // Asegúrate de que todas las rutas usen la raíz correctamente
 });
